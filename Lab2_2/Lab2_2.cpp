b79ae95d756b9f457d4a8595a114da3df0956af3
Никита <@DESKTOP-0N74OFC>
2023-09-29 11:13:05 +0700

    Добавьте файлы проекта.

---
+++ Lab2_2/Lab2_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<locale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a;
+	cout << "\n Введите трёхзначное число";
+	cin >> a;
+	int sot = a / 100;
+	int des = a / 10 % 10;
+	int ed = a % 10;
+	int b = sot;
+	sot = des;
+	des = b;
+	sot = sot * 100;
+	des = des * 10;
+	b = sot + des + ed;
+	cout << b;
+}
